474739933d3190b517d7ff9791e3fb5b1154bee7
scoskey <scoskey@gmail.com>
2013-11-12 18:46:01 -0700

    Delete disqus-compat.js

--- disqus-compat.js
+++
@@ -1,9 +0,0 @@
-function typeset() {
-    MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
-}
-
-function disqus_config() {
-    this.callbacks.onNewComment = [typeset];
-    this.callbacks.onInit = [typeset];
-    this.callbacks.onPaginate = [typeset];
-}
